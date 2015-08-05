61997e9d32d1dbf20c1d1e6b5b67b0f90b6edee7
matejv1 <matej.vodopivc@gmail.com>
2015-08-05 21:52:31 +0200

    Added debug server.js configuration ..

--- server.js
+++ server.js
@@ -2,7 +2,16 @@
 
 var fs = require('fs'),
     express = require('express'),
-    http = require('http');
+    http = require('http'),
+    https = require('https');
+
+var https_options = {
+  key: fs.readFileSync('./hostkey.pem'),
+  cert: fs.readFileSync('./hostcert.pem')
+};
+
+var PORT = 8443,
+    HOST = 'localhost';
 
 var app = express();
 
@@ -11,12 +20,20 @@ app.use('/', express.static(__dirname + '/src'));
 app.use('/vendor', express.static(__dirname + '/bower_components'));
 app.use('/template', express.static(__dirname + '/bower_components/ui.bootstrap/template'));
 
+//https://localhost:8443/ui.bootstrap/template/accordion/accordion-group.html
+
+var server = https.createServer(https_options, app)
+                  .listen(PORT, HOST);
+
+
 var httpServer = http.createServer(app);
+var httpOptions = {
+	port: 80,
+	host: 'localhost'
+};
 
-var PORT = process.env.PORT || 80;
-httpServer.listen(PORT);
+httpServer.listen(httpOptions);
 
 console.log('+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+');
-console.log('HTTPS Server listening @ https://%s:%s', PORT);
+console.log('HTTPS Server listening @ https://%s:%s', HOST, PORT);
 console.log('+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+');
-
